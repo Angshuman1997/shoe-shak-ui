6605fc32880219117d08e32fcc987cad8832468a
Angshuman Bardhan <bardhanangshu@gmail.com>
2023-05-07 02:46:57 +0530

    new filter

--- src/components/MainContainer/MainContainer.jsx
+++ src/components/MainContainer/MainContainer.jsx
@@ -4,16 +4,24 @@ import MasonryView from "../MasonryView/MasonryView";
 import AdBanner from "../AdBanner/AdBanner";
 import EqualSizeView from "../EqualSizeView/EqualSizeView";
 import { useSelector } from "react-redux";
+import styled from "styled-components";
 
 const MainContainer = () => {
   const { themeLayout } = useSelector((state) => state);
 
-  const handleClose = () => {
-  };
+  const handleClose = () => {};
 
   return (
     <React.Fragment>
       <AdBanner />
+      <Refine view={themeLayout}>
+        <div>
+          <button>Filter</button>
+        </div>
+        <div>
+          <button>Sort</button>
+        </div>
+      </Refine>
       {themeLayout === "multisize" ? <MasonryView /> : <EqualSizeView />}
       <PopUpModal
         open={false}
@@ -26,3 +34,28 @@ const MainContainer = () => {
 };
 
 export default MainContainer;
+
+const Refine = styled.div`
+  display: flex;
+  -webkit-box-align: center;
+  align-items: center;
+  -webkit-box-pack: center;
+  justify-content: center;
+  column-gap: 3%;
+  margin: 1rem 0 0 0;
+  button {
+    padding: 0.5rem 0.8rem;
+    border: 2px solid #000000;
+    cursor: pointer;
+    width: 5rem;
+    font-size: 0.8rem;
+    font-weight: bold;
+    border-radius: 25px;
+    background: black;
+    color: #ffffff;
+    @media screen and (min-width: 1440px) {
+      font-size: 1.5rem;
+      width: 8rem;
+    }
+  }
+`;
--- src/components/MasonryView/MasonryView.jsx
+++ src/components/MasonryView/MasonryView.jsx
@@ -1,18 +1,6 @@
 import * as React from "react";
 import Box from "@mui/material/Box";
-import Paper from "@mui/material/Paper";
 import Masonry from "@mui/lab/Masonry";
-import { styled } from "@mui/material/styles";
-
-const Label = styled(Paper)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(0.5),
-  textAlign: "center",
-  color: theme.palette.text.secondary,
-  borderBottomLeftRadius: 0,
-  borderBottomRightRadius: 0,
-}));
 
 export default function MasonryView() {
   React.useEffect(() => {
@@ -35,11 +23,10 @@ export default function MasonryView() {
   }, []);
 
   return (
-    <Box sx={{ width: "100%", paddingTop: "1rem"}}>
+    <Box sx={{ width: "100%", paddingTop: "0.5rem"}}>
       <Masonry columns={{ xs: 2, md: 4, xl: 6 }} spacing={2} sx={{ margin: "0px"}}>
         {itemData.map((item, index) => (
           <div key={index}>
-            <Label>{index + 1}</Label>
             <img
               src={`${item.img}?w=162&auto=format`}
               srcSet={`${item.img}?w=162&auto=format&dpr=2 2x`}
