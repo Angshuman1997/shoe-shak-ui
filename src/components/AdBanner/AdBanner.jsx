d6af53434d47b33b2d6309fb58e59bd40eaa9092
Angshuman Bardhan <bardhanangshu@gmail.com>
2023-05-07 16:30:01 +0530

    banner arrow

--- src/components/AdBanner/AdBanner.jsx
+++ src/components/AdBanner/AdBanner.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 function AdBanner() {
   const colors = ["#0088FE", "#00C49F", "#e4e716", "#ff4d00"];
@@ -46,13 +48,16 @@ function AdBanner() {
               setHovered(true);
               setIndex(index);
             }}
-            onMouseOut={()=>setHovered(false)}
+            onMouseOut={() => setHovered(false)}
           >
             Hi
           </SlideItem>
         ))}
       </SliderContent>
       <SlideDotContent>
+        <button className="arrow" onClick={()=>setIndex(index === 0 ? colors.length - 1 : index-1)}>
+          <ArrowBackIosIcon />
+        </button>
         {colors.map((_, idx) => (
           <SlideDotBtn
             key={idx}
@@ -66,6 +71,9 @@ function AdBanner() {
             <SlideDot active={index === idx} />
           </SlideDotBtn>
         ))}
+        <button className="arrow" onClick={()=>setIndex(index === colors.length - 1 ? 0 : index+1)}>
+          <ArrowForwardIosIcon />
+        </button>
       </SlideDotContent>
     </Container>
   );
@@ -83,6 +91,47 @@ const SliderContent = styled.div`
   transition: ease 1000ms;
   transform: ${(props) =>
     props.indexVal ? `translate3d(${props.indexVal}%, 0, 0)` : ""};
+`;
+
+const SlideDotContent = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  margin: 1rem 0 1rem 0;
+  button.arrow {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 2.5rem;
+    height: 2rem;
+    background: transparent;
+    border: none;
+    cursor: pointer;
+    color: #000000;
+    &:hover {
+      color: #837575;
+    }
+  }
+`;
+
+const SlideDotBtn = styled.button`
+  border: none;
+  cursor: pointer;
+  padding: 0;
+  background: none;
+  margin: 0 0.4rem;
+`;
+
+const SlideDot = styled.div`
+  display: inline-block;
+  height: 1.25rem;
+  width: 1.25rem;
+  border-radius: 50%;
+  background-color: ${(props) => (props.active ? "#3a3838" : "#c4c4c4")};
+  @media screen and (max-width: 800px) {
+    height: 0.5rem;
+    width: 0.5rem;
+  }
 `;
 
 const SlideItem = styled.div`
@@ -113,28 +162,4 @@ const SlideItem = styled.div`
   border-radius: 0.3rem;
   background: ${(props) =>
     props?.backgroundColor ? props?.backgroundColor : "#ffffff"};
-`;
-
-const SlideDotContent = styled.div`
-  text-align: center;
-`;
-
-const SlideDotBtn = styled.button`
-  border: none;
-  cursor: pointer;
-  padding: 0;
-  background: none;
-`;
-
-const SlideDot = styled.div`
-  display: inline-block;
-  height: 1.25rem;
-  width: 1.25rem;
-  border-radius: 50%;
-  margin: 15px 7px 0px;
-  background-color: ${(props) => (props.active ? "#3a3838" : "#c4c4c4")};
-  @media screen and (max-width: 800px) {
-    height: 0.5rem;
-    width: 0.5rem;
-  }
 `;
